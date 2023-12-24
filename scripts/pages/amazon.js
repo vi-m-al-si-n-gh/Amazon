d92b0ed9661ac65d8f0e1331ab06944627197d4b
vi-m-al-si-n-gh <121181159+vi-m-al-si-n-gh@users.noreply.github.com>
2023-12-24 11:33:12 +0530

    Delete scripts directory

--- scripts/pages/amazon.js
+++
@@ -1,9 +0,0 @@
-import { AmazonHeader } from '../components/shared/AmazonHeader.js';
-import { ProductsGrid } from '../components/amazon/ProductsGrid.js';
-import { products } from '../data/products.js';
-
-products.loadFromBackend().then(() => {
-    const amazonHeader = new AmazonHeader('.js-amazon-header').create();
-    const productsGrid = new ProductsGrid('.js-products-grid').create();
-    productsGrid.setAmazonHeader(amazonHeader);
-});
--- scripts/utils/DomUtils.js
+++
@@ -1,6 +0,0 @@
-export class DomUtils {
-    static removeElement(element) {
-        if (!element || !element.parentNode) return;
-        element.parentNode.removeChild(element);
-    }
-}
